8cd46355ad6fc638d1e69190ff91648f6320d0c2
R7Murat <arseven.murat@gmail.com>
2026-06-11 14:54:01 +0300

    Fix EKS private endpoint for node group

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -91,7 +91,7 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids              = aws_subnet.public[*].id
     endpoint_public_access  = true
-    endpoint_private_access = false
+    endpoint_private_access = true          # nodes join via private endpoint within VPC
     public_access_cidrs     = [var.allowed_api_cidr]
   }
 
